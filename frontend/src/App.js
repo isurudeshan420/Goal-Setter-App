e48c574f4e3cbd7fae349c5c8d9c94ff1301cfe9
Isuru Deshan <105171416+isurudeshan20@users.noreply.github.com>
2023-10-17 15:07:53 +0530

    added header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
-      <h1>Goal Setter</h1>
-      
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Header /> 
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<Dashboard />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
